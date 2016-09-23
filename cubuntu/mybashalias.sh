19ef17829e4c84e9c7cd60d0f74153844a2e3d28
chetanvvdn <chetan.kumarsanga@vvdntech.com>
2016-09-23 15:01:34 +0530

    Removed obsolete script alias defs

--- cubuntu/mybashalias.sh
+++ cubuntu/mybashalias.sh
@@ -17,20 +17,6 @@ alias jiyo='sudo reboot'
 alias detag='rm -rf cscope.out; rm -rf tags'
 alias retag='rm -rf cscope.out; rm -rf tags; cscope -Rb; ctags -R'
 
-# Mounting partitions
-alias c1y='/home/vchn075/ChetaN/snips/cubuntu/mounters/mntC1.sh 1 && cd /media/vchn075/C1'
-alias c1n='cd ; /home/vchn075/ChetaN/snips/cubuntu/mounters/mntC1.sh 0'
-alias c2y='/home/vchn075/ChetaN/snips/cubuntu/mounters/mntC2.sh 1 && cd /media/vchn075/C2'
-alias c2n='cd ; /home/vchn075/ChetaN/snips/cubuntu/mounters/mntC2.sh 0'
-
-# 'Hugos' partition
-alias hugo='/home/vchn075/ChetaN/snips/cubuntu/mounters/mntHugos.sh 1'
-alias hugod='/home/vchn075/ChetaN/snips/cubuntu/mounters/mntHugos.sh 1 && cd /media/vchn075/Hugos/fslu_nvme_sdk/ear5/fslu_nvme_sdk_ear5/Layerscape2-SDK-20150828-yocto'
-alias hngo='cd ; /home/vchn075/ChetaN/snips/cubuntu/mounters/mntHugos.sh 0'
-
-# Process completion waiter
-alias pbar='/home/vchn075/ChetaN/snips/cubuntu/misc/pbar.sh'
-
 ###############################
 #### path specific aliases ####
 ###############################
@@ -46,7 +32,6 @@ alias wrk='cd /home/vchn075/ChetaN/snips'
 alias iovs='cd /home/vchn075/ChetaN/Projects/xilu_iovs'
 
 # FSLU_NVME #
-alias fslu='cd /media/vchn075/Hugos/fslu_nvme_sdk/ear5/fslu_nvme_sdk_ear5/Layerscape2-SDK-20150828-yocto/build_ls2085aissd_release/tmp/work/ls2085aissd-fsl-linux/linux-ls2-sdk/4.0-r0/git'
 alias sfslu='cd /home/vchn075/ChetaN/Projects/fslu_nvme'
 
 # NTGU_W710 #
