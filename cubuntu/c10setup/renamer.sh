764c65a414a548991976be2a89a6a97cf5a7b359
chetanc10 <chetan.kumarsanga@gmail.com>
2017-08-08 15:38:51 +0530

    Simplified and added more substitution patterns

--- cubuntu/c10setup/renamer.sh
+++ cubuntu/c10setup/renamer.sh
@@ -5,40 +5,38 @@ Usage="Usage: renamer.sh <src-directory>"
 [[ -z "$1" ]] && echo $Usage && exit -1
 
 search_dir=$@
+[ ! -d "$1" ] && echo "'$search_dir' is not a valid directory!" && exit -1
 
-despacer () {
-	#echo "Working on Folder: $1"
+declare -a rcmd_names=("despace" "desquare" "deparenthesise" "dedash")
+
+declare -A rcmd_usage=(["despace"]="Replace all occurrences of ' '(space) with '_'(underscore)" ["desquare"]="Remove all occurrences of '[anything-between]'" ["deparenthesise"]="Remove all occurrences of '(anything-between)" ["dedash"]="Replace all occurrences of '_-_' with '_'")
+
+declare -A rcmd=(["despace"]="s/ /_/g" ["desquare"]="s/\[.*\]//g" ["deparenthesise"]="s/\(.*\)//g" ["dedash"]="s/_-_/_/g")
+
+renamer () {
+	substituter="${rcmd["$2"]}"
+	echo "Folder: $1"
 	cd "$1"
 	local entry
 	for entry in ./*
 do
-	[[ -d "$entry" ]] && despacer "$entry"
-	#echo "despacing file: $entry in $PWD"
-	rename 's/ /_/g' "$entry"
+	[[ -d "$entry" ]] && [[ "$entry" != "." ]] && renamer "$entry" $2
+	#rename -n "$substituter" "$entry"
+	rename "$substituter" "$entry"
 done
 	#echo "going back"
-	cd ..
+	[ "$1" != "." ] && [ "$1" != ".." ] && cd ..
 }
 
-debracker () {
-	#echo "Working on Folder: $1"
-	cd "$1"
-	local entry
-	for entry in ./*
+for rcmd_entry in "${rcmd_names[@]}"
 do
-	[[ -d "$entry" ]] && debracker "$entry"
-	#echo "debracking file: $entry in $PWD"
-	rename 's/\[.*\]//g' "$entry"
+	echo -e "\n$rcmd_entry: ${rcmd_usage["$rcmd_entry"]}"
+	echo -ne "Want to $rcmd_entry?(y|n): "
+	read answer
+	[[ "$answer" == "y" ]] && renamer "$search_dir" $rcmd_entry && echo "$rcmd_entry operation is done!"
 done
-	#echo "going back"
-	cd ..
-}
 
-echo -n "Want despacer? (y|n): "
-read answer
-[[ "$answer" == "y" ]] && despacer "$search_dir" && echo "despacer is done!"
+echo -ne "\n"
 
-echo -n "Want debracker? (y|n): "
-read answer
-[[ "$answer" == "y" ]] && debracker "$search_dir" && echo "debracker is done!"
+exit 0
 
