95182bd137777c8cab7f9bc6d7f909e5bfe2c2b5
chetanc10 <chetan.kumarsanga@gmail.com>
2017-08-19 15:50:43 +0530

    Added xpad-killer

--- cubuntu/c10setup/lpmode.sh
+++ cubuntu/c10setup/lpmode.sh
@@ -5,6 +5,7 @@
 echo "killing processes.."
 killall artha 2>/dev/null && echo "artha"
 killall tomboy 2>/dev/null && echo "tomboy"
+killall xpad 2>/dev/null && echo "xpad"
 killall dropbox 2>/dev/null && echo "dropbox"
 echo "Killing sshd and ssh-agent. Restart them by: sudo service sshd start && sudo ssh-agent"
 service stop sshd 2>/dev/null && echo "sshd"
