6ef9225e5508636edf85de9a7225b72fb36f85ef
chetanc10 <chetan.kumarsanga@gmail.com>
2017-08-30 14:53:27 +0530

    Corrected and updated addr2line usage example

--- csamples/ex_addr2line.c
+++ csamples/ex_addr2line.c
@@ -7,88 +7,96 @@
 
 #include <execinfo.h>
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
+
+#define BUITLTINS 0
 
 #define BT_BUF_SIZE 100
 
-char *pname;
+#define USING_BASH_SYSTEM_FUNC 0
 
-int addr2line(char const * const program_name, void const * const addr)
+char *progname;
+
+void my_addr2line(char *progname, void *addr)
 {
-	char addr2line_cmd[512] = {0};
+    char addr2line_cmd[512] = {0};
 
-	/* have addr2line map the address to the relent line in the code */
-#ifdef __APPLE__
-	/* apple does things differently... */
-	sprintf(addr2line_cmd,"atos -o %.256s %p", program_name, addr); 
+    sprintf(addr2line_cmd,"addr2line -f -p -e %.256s %p", progname, addr); 
+#if USING_BASH_SYSTEM_FUNC
+    system (addr2line_cmd);
 #else
-	sprintf(addr2line_cmd,"addr2line -f -p -e %.256s %p", program_name, addr); 
+    {
+	FILE *fp;
+	fp = popen(addr2line_cmd, "r");
+	if (fp) {
+	    char buf[128];
+	    while (fgets(buf, sizeof(buf), fp)) {
+		if (!strstr (buf, "??"))
+		    printf("%s", buf); 
+	    }
+	} else {
+	    perror (addr2line_cmd);
+	}
+    }
 #endif
-
-	/* This will print a nicely formatted string specifying the
-	   function and source line of the address */
-	return system (addr2line_cmd);
 }
 
-void myfunc3(void)
+void strackers(void)
 {
-	int j, nptrs;
-	void *buffer[BT_BUF_SIZE];
-	void **strings;
-
-	nptrs = backtrace(buffer, BT_BUF_SIZE);
-	printf("backtrace() returned %d addresses\n", nptrs);
-
-	/* The call backtrace_symbols_fd(buffer, nptrs, STDOUT_FILENO)
-	   would produce similar output to the following: */
-
-	strings = (void **)backtrace_symbols(buffer, nptrs);
-	if (strings == NULL) {
-		perror("backtrace_symbols");
-		exit(EXIT_FAILURE);
-	}
-
-	for (j = 0; j < nptrs - 2; j++) {
-#if 1
-		printf("%s\n", (char *)strings[j]);
-		if (0)
-		{
-			char ch;
-			printf ("next? <c>: ");
-			scanf (" %c", &ch);
-		}
+#if BUITLTINS
+    /* Might not be portable/flexible, but it's another way to do it! */
+    my_addr2line(__builtin_return_address(0));
+    my_addr2line(__builtin_return_address(1));
+    my_addr2line(__builtin_return_address(2));
+    my_addr2line(__builtin_return_address(3));
 #else
-		/*printf("%s\n", (char *)strings[j]);*/
-		if (addr2line (pname, buffer[j])) {
-			printf ("couldn't get human readable lines from backtrace\n");
-		}
-#endif
-	}
+    int j, nptrs;
+    void *buffer[BT_BUF_SIZE];
+    void **strings;
 
-	free(strings);
+    nptrs = backtrace(buffer, BT_BUF_SIZE);
+    printf("backtrace() returned %d addresses\n", nptrs);
+
+    /* The call backtrace_symbols_fd(buffer, nptrs, STDOUT_FILENO)
+       would produce similar output to the following: */
+
+    strings = (void **)backtrace_symbols(buffer, nptrs);
+    if (strings == NULL) {
+	perror("backtrace_symbols");
+	exit(EXIT_FAILURE);
+    }
+
+    for (j = 0; j < nptrs - 2; j++) {
+	/*printf("%s\n", (char *)strings[j]);*/
+	my_addr2line (progname, buffer[j]);
+    }
+
+    free(strings);
+#endif
 }
 
 static void myfunc2(void)
 {
-	myfunc3();
+    strackers();
 }
 
 void myfunc(int ncalls)
 {
-	if (ncalls > 1)
-		myfunc(ncalls - 1);
-	else
-		myfunc2();
+    if (ncalls > 1)
+	myfunc(ncalls - 1);
+    else
+	myfunc2();
 }
 
 int main(int argc, char *argv[])
 {
-	if (argc != 2) {
-		fprintf(stderr, "%s num-calls\n", argv[0]);
-		exit(EXIT_FAILURE);
-	}
+    if (argc != 2) {
+	fprintf(stderr, "%s num-calls\n", argv[0]);
+	exit(EXIT_FAILURE);
+    }
 
-	pname = argv[0];
-	myfunc(atoi(argv[1]));
-	exit(EXIT_SUCCESS);
+    progname = argv[0];
+    myfunc(atoi(argv[1]));
+    exit(EXIT_SUCCESS);
 }
