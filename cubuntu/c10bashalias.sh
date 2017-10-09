10c37c4ab714260abf27d8d7ad5c794cf49ba1a9
chetanc10 <chetan.kumarsanga@gmail.com>
2017-10-09 14:37:53 +0530

    Removed project path bash aliases

--- cubuntu/c10bashalias.sh
+++ cubuntu/c10bashalias.sh
@@ -13,27 +13,9 @@
 alias detag='rm -rf cscope.out; rm -rf tags'
 alias retag='cscope -Rb && ctags -R'
 
-###############################
-#### path specific aliases ####
-###############################
-
 ### Miscellaneous regulars ####
 alias src='cd /home/vchn075/ChetaN/refs'
 alias scrap='cd /home/vchn075/ChetaN/junk/scrap'
 alias wrk='cd /home/vchn075/ChetaN/snips'
 alias gitst='git status'
-
-### Current projects ###
-
-# WAC_720 #
-alias wacu='cd /home/vchn075/ChetaN/projects/wac_720'
-
-# HFCI_WLCD #
-alias wlcd='cd /home/vchn075/ChetaN/projects/hfci_wlcd'
-
-# XILU_iOVS #
-alias iovs='cd /home/vchn075/ChetaN/projects/xilu_iovs'
-
-# FSLU_NVME #
-alias sfslu='cd /home/vchn075/ChetaN/projects/fslu_nvme'
 
