0214fc959401731f112de295378d3de746464e1b
chetanc10 <chetan.kumarsanga@gmail.com>
2018-03-24 13:39:32 +0530

    Added 1 more cmd-alias

--- cubuntu/c10bashalias.sh
+++ cubuntu/c10bashalias.sh
@@ -1,15 +1,9 @@
 
 ###############################
-###############################
 ## my bash alias definitions ##
 ###############################
-###############################
 
-###############################
-#### multi-purpose aliases ####
-###############################
-
-# source code browsing
+### source code browsing ###
 alias detag='rm -rf cscope.out; rm -rf tags'
 alias retag='cscope -Rb && ctags -R'
 
@@ -17,8 +11,8 @@ alias retag='cscope -Rb && ctags -R'
 alias src='cd /home/$USER/ChetaN/refs'
 alias scrap='cd /home/$USER/ChetaN/junk/scrap'
 alias snips='cd /home/$USER/ChetaN/snips'
+alias sim='cd /home/$USER/ChetaN/simeth'
 alias gitst='git status'
 alias gitm='git checkout master'
 alias gitc='git checkout'
 alias gitp='git pull'
-
