4b94fa96e3cb4841785eb97d8c4956ea5685f6f1
ChetaN KS <chetan.kumarsanga@gmail.com>
2018-05-23 19:11:52 +0530

    Cleaned xdump to be a includable header file and simplified API

--- csamples/xdump.h
+++ csamples/xdump.h
@@ -7,20 +7,20 @@
 #include <inttypes.h>
 #include <syslog.h>
 
-#define USING_SYSLOG 1
+#define USING_SYSLOG 0
 
 /*define as 1 if we need the functions to log the buffer as hex-dump, 0 for nothing*/
 #define USING_XDUMP 1
 
 #define MAX_SBUF_LEN 8096 /*8KB*/
 
-#define _FMT_8   1
+#define FMTB_8   1
 #define FMT_8    "%"PRIx8
-#define _FMT_16  2
+#define FMTB_16  2
 #define FMT_16   "%"PRIx16
-#define _FMT_32  3
+#define FMTB_32  3
 #define FMT_32   "%"PRIx32
-#define _FMT_64  4
+#define FMTB_64  4
 #define FMT_64   "%"PRIx64
 
 #if USING_XDUMP
@@ -47,22 +47,22 @@ static inline void xdump (char *bufname, uint8_t *buf, uint32_t len, int fmt)
 	uint64_t *buf64;
 
 	switch (fmt) {
-		case _FMT_8:
+		case FMTB_8:
 			for (i = 0, buf8 = (uint8_t *)buf; i < len; i++) {
 				n += sprintf (sbuf + n, FMT_8" ", buf8[i]);
 			}
 			break;
-		case _FMT_16:
+		case FMTB_16:
 			for (i = 0, buf16 = (uint16_t *)buf; i < len; i++) {
 				n += sprintf (sbuf + n, FMT_16" ", buf16[i]);
 			}
 			break;
-		case _FMT_32:
+		case FMTB_32:
 			for (i = 0, buf32 = (uint32_t *)buf; i < len; i++) {
 				n += sprintf (sbuf + n, FMT_32" ", buf32[i]);
 			}
 			break;
-		case _FMT_64:
+		case FMTB_64:
 			for (i = 0, buf64 = (uint64_t *)buf; i < len; i++) {
 				n += sprintf (sbuf + n, FMT_64" ", buf64[i]);
 			}
@@ -76,34 +76,31 @@ static inline void xdump (char *bufname, uint8_t *buf, uint32_t len, int fmt)
 	printf ("%s: %s\n", bufname, sbuf);
 #endif
 }
-#endif
 
 /**
  * @function           : xdump_xx (xx = 8 or 16 or 32 or 64 - bits)
  * @brief              : xdump given a named buffer in given int format
  *                       in 8 or 16 or 32 or 64 bit integer format as per name
  * @caller             : anyone who needs to debug buffer sent/received via n/w
- * @input              : char *bufname - name of the buffer for proper identity
- *                       uint8_t *buf - buffer pointer
+ * @input              : uint8_t *buf - buffer pointer
  *                       uint32_t len - length of buf
  * @output             : none
  * @return             : none
  */
-#if USING_XDUMP
-#define xdump_8b(bufname, buf, len) \
-	xdump (bufname, buf, len/sizeof (uint8_t), _FMT_8)
-#define xdump_16b(bufname, buf, len) \
-	xdump (bufname, buf, len/sizeof (uint16_t), _FMT_16)
-#define xdump_32b(bufname, buf, len) \
-	xdump (bufname, buf, len/sizeof (uint32_t), _FMT_32)
-#define xdump_64b(bufname, buf, len) \
-	xdump (bufname, buf, len/sizeof (uint64_t), _FMT_64)
+#define xdump_8b(buf, len) \
+	xdump (#buf, buf, len/sizeof (uint8_t), FMTB_8)
+#define xdump_16b(buf, len) \
+	xdump (#buf, buf, len/sizeof (uint16_t), FMTB_16)
+#define xdump_32b(buf, len) \
+	xdump (#buf, buf, len/sizeof (uint32_t), FMTB_32)
+#define xdump_64b(buf, len) \
+	xdump (#buf, buf, len/sizeof (uint64_t), FMTB_64)
 
 #else
-#define xdump_8b(bufname, buf, len)
-#define xdump_16b(bufname, buf, len)
-#define xdump_32b(bufname, buf, len)
-#define xdump_64b(bufname, buf, len)
+#define xdump_8b(buf, len)
+#define xdump_16b(buf, len)
+#define xdump_32b(buf, len)
+#define xdump_64b(buf, len)
 
 #endif /*USING_XDUMP*/
 
